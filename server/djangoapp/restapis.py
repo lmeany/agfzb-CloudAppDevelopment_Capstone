078ae5488dcff425d655d3187a564349a08e0723
lmeany <147493623+lmeany@users.noreply.github.com>
2023-11-14 18:01:20 +0400

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -56,73 +56,8 @@ def get_dealers_from_cf(url, **kwargs):
 # def get_dealer_by_id_from_cf(url, dealerId):
 # - Call get_request() with specified arguments
 # - Parse JSON results into a DealerView object list
-def get_dealer_reviews_from_cf(url, dealer_id):
-    results = []
-    # Perform a GET request with the specified dealer id
-    json_result = get_request(url, dealerId=dealer_id)
-
-    if json_result:
-        # Get all review data from the response
-        reviews = json_result["body"]["data"]["docs"]
-        # For every review in the response
-        for review in reviews:
-            # Create a DealerReview object from the data
-            # These values must be present
-            review_content = review["review"]
-            id = review["_id"]
-            name = review["name"]
-            purchase = review["purchase"]
-            dealership = review["dealership"]
-
-            try:
-                # These values may be missing
-                car_make = review["car_make"]
-                car_model = review["car_model"]
-                car_year = review["car_year"]
-                purchase_date = review["purchase_date"]
-
-                # Creating a review object
-                review_obj = DealerReview(dealership=dealership, id=id, name=name, 
-                                          purchase=purchase, review=review_content, car_make=car_make, 
-                                          car_model=car_model, car_year=car_year, purchase_date=purchase_date
-                                          )
-
-            except KeyError:
-                print("Something is missing from this review. Using default values.")
-                # Creating a review object with some default values
-                review_obj = DealerReview(
-                    dealership=dealership, id=id, name=name, purchase=purchase, review=review_content)
-
-            # Analysing the sentiment of the review object's review text and saving it to the object attribute "sentiment"
-            review_obj.sentiment = analyze_review_sentiments(review_obj.review)
-            print(f"sentiment: {review_obj.sentiment}")
-
-            # Saving the review object to the list of results
-            results.append(review_obj)
-
-    return results
-
 
 # def get_dealer_by_id_from_cf(url, dealerId):
-def get_dealer_by_id_from_cf(url, dealerId):
-    json_result = get_request(url, id=id)
-    dealer_obj = None
-    print('JSON RESULT:', json_result)
-    dealer_doc = json_result
-    if json_result:
-        for dealer_data in json_result:
-            if dealer_data.get('doc', {}).get('id') == id:
-                dealer_doc = dealer_data['doc']
-                dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"],
-                                       id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
-                                       full_name=dealer_doc["full_name"],
-                                       st=dealer_doc["st"], zip=dealer_doc["zip"])
-                break  # Exit the loop if a valid dealer is found
-            else:
-                print("DOC is not found!")
-    print('DEALER OBJ:', dealer_obj)
-    return dealer_obj
-
 
 # Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
 # def analyze_review_sentiments(text):
